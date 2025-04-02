50f1dab7b56234a1dd94ee8f93764f2bc0c567ca
chernikovaG <chernikova.gv@edu.spbstu.ru>
2025-04-02 03:15:33 +0300

    moved the filtering to the database level

--- src/main/java/ru/tbank/bookit/book_it_backend/repository/BookingRepository.java
+++ src/main/java/ru/tbank/bookit/book_it_backend/repository/BookingRepository.java
@@ -1,8 +1,23 @@
 package ru.tbank.bookit.book_it_backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.tbank.bookit.book_it_backend.model.Booking;
 
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+    @Query("SELECT b FROM Booking b WHERE b.userId = :userId AND b.startTime < :now AND b.endTime > :now")
+    List<Booking> findCurrentBookingsByUser(@Param("userId") Long userId, @Param("now") LocalDateTime now);
+
+    @Query("SELECT b FROM Booking b WHERE b.userId = :userId AND b.startTime > :now")
+    List<Booking> findFutureBookingsByUser(@Param("userId") Long userId, @Param("now") LocalDateTime now);
+
+    @Query("SELECT b FROM Booking b WHERE b.userId = :userId AND b.endTime < :now")
+    List<Booking> findPastBookingsByUser(@Param("userId") Long userId, @Param("now") LocalDateTime now);
+
+    @Query("SELECT b FROM Booking b WHERE b.userId = :userId")
+    List<Booking> findAllByUser(@Param("userId") Long userId);
 }
