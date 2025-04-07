f4e8404a531dcb425e02d541cf1bf8bc3ee34a23
chernikovaG <chernikova.gv@edu.spbstu.ru>
2025-04-07 14:40:51 +0300

    added DayStatus

---
+++ src/main/java/ru/tbank/bookit/book_it_backend/model/DayStatus.java
@@ -0,0 +1,22 @@
+package ru.tbank.bookit.book_it_backend.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum DayStatus {
+    WORKING_DAY("Рабочий день"),
+    WEEKEND("Выходной день"),
+    HOLIDAY("Государственный праздник"),
+
+    TECHNICAL_WORK("Технические работы"),
+    SANITARY_DAY("Санитарный день"),
+    PRIVATE_EVENT("Частное мероприятие"),
+
+    WEATHER_EMERGENCY("Чрезвычайная погодная ситуация"),
+    QUARANTINE("Карантинные мероприятия"),
+    ELECTRICITY_OUTAGE("Отключение электроэнергии");
+
+    private final String description;
+}
