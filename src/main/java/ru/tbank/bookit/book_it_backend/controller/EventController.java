460e40f43a47687177ba682224066128affedfc0
Kastelia <kristinazak150@gmail.com>
2025-04-08 18:25:53 +0300

    event operations in event

--- src/main/java/ru/tbank/bookit/book_it_backend/controller/EventController.java
+++ src/main/java/ru/tbank/bookit/book_it_backend/controller/EventController.java
@@ -1,5 +1,7 @@
 package ru.tbank.bookit.book_it_backend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,12 +25,18 @@ public class EventController {
         this.eventRepository = eventRepository;
     }
 
+    @Operation(
+            description = "Returns information in the list format about all events"
+    )
     @GetMapping("/all")
     public ResponseEntity<List<Event>> getAllEvents() {
         List<Event> event = eventService.findAll();
         return ResponseEntity.ok(event);
     }
 
+    @Operation(
+            description = "Returns information in list format about all events for a specific tag"
+    )
     @GetMapping("/by-tags")
     public ResponseEntity<List<Event>> getAllEventsByTags(
             @RequestParam(required = true) Set<NewsTag> tags) {
@@ -36,12 +44,18 @@ public class EventController {
         return ResponseEntity.ok(event);
     }
 
+    @Operation(
+            description = "Returns information about the event status by user"
+    )
     @GetMapping("/status")
     public ResponseEntity<EventStatus> getStatusById(@RequestParam long userId, @RequestParam long eventId){
         Event event = eventService.findById(eventId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Event not found"));
         return ResponseEntity.ok(eventService.findStatusById(userId, event));
     }
 
+    @Operation(
+            description = "Registers (by entering the guest list of the event) the user for this event"
+    )
     @PutMapping("/register")
     public ResponseEntity<Event> addUserInList(@RequestParam long userId, @RequestParam long eventId){
         Event event = eventService.findById(eventId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Event not found"));
@@ -49,6 +63,9 @@ public class EventController {
         return ResponseEntity.ok(event);
     }
 
+    @Operation(
+            description = "returns information about the created event"
+    )
     @PostMapping("/event")
     public ResponseEntity<Event> createEvent(@RequestBody Event event) {
         eventRepository.save(event);
