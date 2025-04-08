2502b2b20fa034c8c7c12b7cc6f9a75482a1560a
Kastelia <kristinazak150@gmail.com>
2025-04-08 18:30:11 +0300

    event operations in home

--- src/main/java/ru/tbank/bookit/book_it_backend/controller/HomeController.java
+++ src/main/java/ru/tbank/bookit/book_it_backend/controller/HomeController.java
@@ -1,5 +1,6 @@
 package ru.tbank.bookit.book_it_backend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +25,9 @@ public class HomeController {
         this.userRepository = userRepository;
     }
 
+    @Operation(
+            description = "returns QR code in string format"
+    )
     @GetMapping("/qr")
     public ResponseEntity<?> getUserQrCode(@RequestParam Long userId) {
         User user = userRepository.findById(userId)
@@ -37,24 +41,36 @@ public class HomeController {
         }
     }
 
+    @Operation(
+            description = "returns information in the list format about current bookings"
+    )
     @GetMapping("/bookings/current")
     public ResponseEntity<List<Booking>> getCurrentBookings(@RequestParam Long userId) {
         List<Booking> bookings = homeService.getCurrentBookings(userId);
         return ResponseEntity.ok(bookings);
     }
 
+    @Operation(
+            description = "returns information in the list format about future bookings"
+    )
     @GetMapping("/bookings/future")
     public ResponseEntity<List<Booking>> getFutureBookings(@RequestParam Long userId) {
         List<Booking> bookings = homeService.getFutureBookings(userId);
         return ResponseEntity.ok(bookings);
     }
 
+    @Operation(
+            description = "returns information in the list format about past bookings"
+    )
     @GetMapping("/bookings/past")
     public ResponseEntity<List<Booking>> getPastBookings(@RequestParam Long userId) {
         List<Booking> bookings = homeService.getPastBookings(userId);
         return ResponseEntity.ok(bookings);
     }
 
+    @Operation(
+            description = "returns a string about successful remote booking"
+    )
     @DeleteMapping("/booking/{bookingId}")
     public ResponseEntity<String> cancelBooking(@PathVariable long bookingId) {
         homeService.cancelBooking(bookingId);
