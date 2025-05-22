fa3269d597de407b6cc5efadda72c7ce6f71a8ab
chernikovaG <chernikova.gv@edu.spbstu.ru>
2025-05-22 14:28:40 +0300

    added UserDetailsImpl

--- src/main/java/ru/tbank/bookit/book_it_backend/mapper/AreaMapper.java
+++ src/main/java/ru/tbank/bookit/book_it_backend/mapper/AreaMapper.java
@@ -1,24 +1,53 @@
 package ru.tbank.bookit.book_it_backend.mapper;
 
-import org.mapstruct.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.tbank.bookit.book_it_backend.DTO.AreaResponse;
+import ru.tbank.bookit.book_it_backend.DTO.CreateBookingRequest;
 import ru.tbank.bookit.book_it_backend.model.Area;
 import ru.tbank.bookit.book_it_backend.model.AreaFeature;
 
-import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+/**
+ * Интерфейс для маппинга между сущностью Area и DTO
+ */
 @Mapper(componentModel = "spring")
 public interface AreaMapper {
 
-    @Mapping(target = "type", source = "type")
-    @Mapping(target = "features", expression = "java(mapFeatures(area.getFeatures()))")
+    /**
+     * Преобразует сущность Area в AreaResponse
+     */
     AreaResponse toAreaResponse(Area area);
 
-    List<AreaResponse> toAreaResponseList(List<Area> areas);
+    /**
+     * Преобразует сущность Area в CreateBookingRequest
+     */
+    @Mapping(target = "areaId", source = "id")
+    @Mapping(target = "userId", ignore = true)
+    @Mapping(target = "timePeriods", ignore = true)
+    @Mapping(target = "quantity", constant = "1")
+    CreateBookingRequest toDTO(Area area);
 
-    default Set<String> mapFeatures(AreaFeature features) {
-        if (features == null) return null;
-        return Set.of(features.name());
+    /**
+     * Преобразует CreateBookingRequest в сущность Area
+     */
+    @Mapping(target = "id", source = "areaId")
+    @Mapping(target = "name", ignore = true)
+    @Mapping(target = "description", ignore = true)
+    // Добавьте здесь игнорирование других полей Area, которых нет в CreateBookingRequest
+    Area toEntity(CreateBookingRequest dto);
+
+    /**
+     * Метод для преобразования Set<AreaFeature> в Set<String>
+     */
+    default Set<String> map(Set<AreaFeature> features) {
+        if (features == null) {
+            return null;
+        }
+        return features.stream()
+                .map(Enum::name)
+                .collect(Collectors.toSet());
     }
 }
