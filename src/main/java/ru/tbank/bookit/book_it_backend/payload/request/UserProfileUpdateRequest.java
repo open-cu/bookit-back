d9bf83664e59504a419aecaf573ecfcb6bf1ae75
chernikovaG <chernikova.gv@edu.spbstu.ru>
2025-06-29 17:35:34 +0300

    Added a check for the phone number format

--- src/main/java/ru/tbank/bookit/book_it_backend/payload/request/UserProfileUpdateRequest.java
+++ src/main/java/ru/tbank/bookit/book_it_backend/payload/request/UserProfileUpdateRequest.java
@@ -2,6 +2,7 @@ package ru.tbank.bookit.book_it_backend.payload.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -13,6 +14,7 @@ public class UserProfileUpdateRequest {
     private String lastName;
 
     @NotBlank
+    @Pattern(regexp = "^(\\+7\\d{10}|8\\d{10})$", message = "Incorrect phone number format")
     private String phone;
 
     @Email
