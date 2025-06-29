5da9bcfc0a0dca7eb2a58ee861a3fd1640bf3843
chernikovaG <chernikova.gv@edu.spbstu.ru>
2025-06-29 18:27:05 +0300

    Refactoring

--- src/main/java/ru/tbank/bookit/book_it_backend/payload/response/JwtResponse.java
+++ src/main/java/ru/tbank/bookit/book_it_backend/payload/response/JwtResponse.java
@@ -1,15 +1,18 @@
 package ru.tbank.bookit.book_it_backend.payload.response;
 
+import lombok.Data;
+
 import java.util.UUID;
 
+@Data
 public class JwtResponse {
-    private String token;
-    private String type = "Bearer";
-    private UUID id;
-    private String username;
-    private String email;
-    private String firstName;
-    private String lastName;
+    private final String token;
+    private final String type = "Bearer";
+    private final UUID id;
+    private final String username;
+    private final String email;
+    private final String firstName;
+    private final String lastName;
 
     public JwtResponse(String accessToken, UUID id, String username, String email, String firstName, String lastName) {
         this.token = accessToken;
@@ -17,62 +20,6 @@ public class JwtResponse {
         this.username = username;
         this.email = email;
         this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 }
--- src/main/java/ru/tbank/bookit/book_it_backend/payload/response/MessageResponse.java
+++ src/main/java/ru/tbank/bookit/book_it_backend/payload/response/MessageResponse.java
@@ -1,17 +1,10 @@
 package ru.tbank.bookit.book_it_backend.payload.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class MessageResponse {
     private String message;
-
-    public MessageResponse(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
