ccc43eae4ee9ed72d5b8ad3ea96cf5f20cdc8c36
Ravinskij Vladimir <ravunskiy0412@gmail.com>
2025-07-10 22:53:12 +0300

    Now it returns response from DB

--- src/main/java/ru/tbank/bookit/book_it_backend/controller/AIController.java
+++ src/main/java/ru/tbank/bookit/book_it_backend/controller/AIController.java
@@ -2,6 +2,7 @@ package ru.tbank.bookit.book_it_backend.controller;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.http.*;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,21 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import ru.tbank.bookit.book_it_backend.DTO.AIRequest;
 import ru.tbank.bookit.book_it_backend.DTO.AIRequestBuilder;
-import ru.tbank.bookit.book_it_backend.DTO.AIRequestComponents.MessageDTO;
 import ru.tbank.bookit.book_it_backend.DTO.AIResponse;
 import ru.tbank.bookit.book_it_backend.DTO.RawAIRequest;
-import ru.tbank.bookit.book_it_backend.DTO.AIRequestComponents.CompletionOptionsDTO;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/ai")
 public class AIController {
-    private RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final JdbcTemplate jdbcTemplate;
+
+    public AIController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @PostMapping("/get-sql")
-    public ResponseEntity<AIResponse> getSql(
+    public ResponseEntity<List<Map<String, Object>>> getSql(
             @RequestBody RawAIRequest rawAIRequest
             ) {
         Dotenv dotenv = Dotenv.configure()
@@ -47,9 +51,16 @@ public class AIController {
         ResponseEntity<AIResponse> response = restTemplate.exchange(url, HttpMethod.POST, aiRequestHttpEntity, AIResponse.class);
 
         if (response.getStatusCode() == HttpStatus.OK) {
-            return ResponseEntity.ok(response.getBody());
-        } else {
-            return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+
+            String SQLRequest = response.getBody() != null ? response.getBody().result().alternatives().getFirst().message().text() : null;
+            if (SQLRequest != null) {
+                SQLRequest = SQLRequest.replace("\\n", "\n");
+                SQLRequest = SQLRequest.replace("\\", "");
+                SQLRequest = SQLRequest.replace("```", "");
+                List<Map<String, Object>> results = jdbcTemplate.queryForList(SQLRequest);
+                return ResponseEntity.ok(results);
+            }
         }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
     }
 }
