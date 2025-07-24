90c6902c57eddf468a9f42218744f33e90eef6d6
Ravinskij-Vladimir <ravunskiy0412@gmail.com>
2025-07-25 00:01:53 +0300

    second step in improving swagger docs

--- web-adapter/src/main/java/com/opencu/bookit/adapter/in/web/controller/v1/AdminAreaControllerV1.java
+++ web-adapter/src/main/java/com/opencu/bookit/adapter/in/web/controller/v1/AdminAreaControllerV1.java
@@ -10,6 +10,7 @@ import com.opencu.bookit.application.service.photo.PhotoService;
 import com.opencu.bookit.domain.model.area.AreaModel;
 import com.opencu.bookit.domain.model.area.AreaType;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -25,6 +26,7 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.UUID;
 
+@Tag(name = "admin-area-controller-v-1", description = "For admins only")
 @RestController
 @RequestMapping("/api/v1/areas")
 public class AdminAreaControllerV1 {
@@ -63,7 +65,7 @@ public class AdminAreaControllerV1 {
 
     @PreAuthorize("@securityService.isDev() or " +
             "@securityService.hasRequiredRole(@securityService.getAdmin())")
-    @Operation(summary = "Get area by ID")
+    @Operation(summary = "Get area by id")
     @GetMapping("/{areaId}")
     public ResponseEntity<AreaResponse> getAreaById(@PathVariable UUID areaId) {
         Optional<AreaModel> area = areaService.findById(areaId);
@@ -82,7 +84,10 @@ public class AdminAreaControllerV1 {
 
     @PreAuthorize("@securityService.isDev() or " +
             "@securityService.hasRequiredRole(@securityService.getAdmin())")
-    @Operation(summary = "Create area")
+    @Operation(
+            summary = "Create area. FOR ADMINS ONLY!",
+            description = "Content-type: multipart/form-data, see Postman tests for more details."
+    )
     @PostMapping
     public ResponseEntity<AreaResponse> createArea(
             @RequestPart("createAreaRequest") CreateAreaRequest createAreaRequest,
@@ -122,7 +127,10 @@ public class AdminAreaControllerV1 {
 
     @PreAuthorize("@securityService.isDev() or " +
             "@securityService.hasRequiredRole(@securityService.getAdmin())")
-    @Operation(summary = "Updated area information")
+    @Operation(
+            summary = "Update area information. FOR ADMINS ONLY!",
+            description = "Content-type: multipart/form-data, see Postman tests for more details."
+    )
     @PutMapping("/{areaId}")
     public ResponseEntity<AreaResponse> updateById(
             @PathVariable UUID areaId,
--- web-adapter/src/main/java/com/opencu/bookit/adapter/in/web/dto/request/CreateAreaRequest.java
+++ web-adapter/src/main/java/com/opencu/bookit/adapter/in/web/dto/request/CreateAreaRequest.java
@@ -3,14 +3,44 @@ package com.opencu.bookit.adapter.in.web.dto.request;
 import com.opencu.bookit.domain.model.area.AreaFeature;
 import com.opencu.bookit.domain.model.area.AreaStatus;
 import com.opencu.bookit.domain.model.area.AreaType;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
 public record CreateAreaRequest(
+        @Schema(
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                type = "String"
+        )
         String name,
+
+        @Schema(
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                type = "String"
+        )
         String description,
+
+        @Schema(
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                type = "String"
+        )
         AreaType type,
+
+        @Schema(
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                type = "array"
+        )
         List<AreaFeature> features,
+
+        @Schema(
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                type = "Integer"
+        )
         int capacity,
+
+        @Schema(
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                type = "String"
+        )
         AreaStatus status
 ) {}
