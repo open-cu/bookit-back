7dd06f74eca7c3d5bf9b36720876cf5ffad61a17
Ravinskij-Vladimir <ravunskiy0412@gmail.com>
2025-07-25 21:08:50 +0300

    refactor PhotoService (implemented s3 as a separated adapter)

---
+++ application/src/main/java/com/opencu/bookit/application/port/out/s3/LoadS3Port.java
@@ -0,0 +1,10 @@
+package com.opencu.bookit.application.port.out.s3;
+
+import com.opencu.bookit.domain.model.image.ImageModel;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface LoadS3Port {
+    List<ImageModel> getImagesFromKeys(List<String> keys) throws IOException;
+}
--- application/src/main/java/com/opencu/bookit/application/service/photo/PhotoService.java
+++ application/src/main/java/com/opencu/bookit/application/service/photo/PhotoService.java
@@ -1,104 +1,29 @@
 package com.opencu.bookit.application.service.photo;
 
+import com.opencu.bookit.application.port.out.s3.LoadS3Port;
+import com.opencu.bookit.application.port.out.s3.SaveS3Port;
 import com.opencu.bookit.domain.model.image.ImageModel;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.core.ResponseInputStream;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.http.apache.ApacheHttpClient;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.GetObjectResponse;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Base64;
 import java.util.List;
 
 @Service
 public class PhotoService {
+    private final LoadS3Port loadS3Port;
+    private final SaveS3Port saveS3Port;
 
-    private final String KEY_ID;
-    private final String SECRET_KEY;
-    private final String REGION ;
-    private final String S3_ENDPOINT;
-    private final String BUCKET;
-
-    private final S3Client s3Client;
-
-    public PhotoService(
-            @Value("${yandex-cloud.key-id}") String keyId,
-            @Value("${yandex-cloud.secret-key}") String secretKey,
-            @Value("${yandex-cloud.region}") String region,
-            @Value("${yandex-cloud.s3-endpoint}") String s3Endpoint,
-            @Value("${yandex-cloud.bucket}") String bucket
-    ) {
-        KEY_ID = keyId;
-        SECRET_KEY = secretKey;
-        REGION = region;
-        S3_ENDPOINT = s3Endpoint;
-        BUCKET = bucket;
-        AwsCredentials credentials = AwsBasicCredentials.create(KEY_ID, SECRET_KEY);
-        this.s3Client = S3Client.builder()
-                .httpClient(ApacheHttpClient.create())
-                .region(Region.of(REGION))
-                .endpointOverride(URI.create(S3_ENDPOINT))
-                .credentialsProvider(StaticCredentialsProvider.create(credentials))
-                .build();
+    public PhotoService(LoadS3Port loadS3Port, SaveS3Port saveS3Port) {
+        this.loadS3Port = loadS3Port;
+        this.saveS3Port = saveS3Port;
     }
 
     public List<ImageModel> getImagesFromKeys(List<String> keys) throws IOException {
-        List<ImageModel> imageModels = new ArrayList<>();
-        for (String key : keys) {
-            imageModels.add(getImageFromKey(key));
-        }
-        return imageModels;
-    }
-
-
-    private ImageModel getImageFromKey(String key) throws IOException {
-        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
-                .bucket(BUCKET)
-                .key(key)
-                .build();
-
-        try (ResponseInputStream<GetObjectResponse> responseInputStream = s3Client.getObject(getObjectRequest)) {
-            byte[] bytes = responseInputStream.readAllBytes();
-            GetObjectResponse metadata = responseInputStream.response();
-
-            String base64Data = Base64.getEncoder().encodeToString(bytes);
-
-            return new ImageModel(
-                    key,
-                    metadata.contentType(),
-                    metadata.contentLength(),
-                    metadata.eTag(),
-                    metadata.lastModified().toString(),
-                    base64Data
-            );
-        }
+        return loadS3Port.getImagesFromKeys(keys);
     }
 
     public List<String> upload(List<MultipartFile> photos) throws IOException {
-        List<String> keys = new ArrayList<>();
-        for (MultipartFile photo : photos) {
-            String key = photo.getOriginalFilename();
-            PutObjectRequest request = PutObjectRequest.builder()
-                    .bucket(BUCKET)
-                    .key(key)
-                    .contentType(photo.getContentType())
-                    .build();
-
-            s3Client.putObject(request, RequestBody.fromBytes(photo.getBytes()));
-            keys.add(key);
-        }
-        return keys;
+        return saveS3Port.upload(photos);
     }
 }
