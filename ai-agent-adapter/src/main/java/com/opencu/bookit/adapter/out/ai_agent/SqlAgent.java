5b191a878f6bd86d48fc9e77b90b5769f2a0473d
Ravinskij-Vladimir <ravunskiy0412@gmail.com>
2025-07-25 23:23:04 +0300

    yet-another improvement of the ai-agent

--- ai-agent-adapter/src/main/java/com/opencu/bookit/adapter/out/ai_agent/SqlAgent.java
+++ ai-agent-adapter/src/main/java/com/opencu/bookit/adapter/out/ai_agent/SqlAgent.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.opencu.bookit.adapter.out.ai_agent.dto.response.SqlResponse;
 import com.opencu.bookit.application.port.out.ai.LoadSqlPort;
 import lombok.RequiredArgsConstructor;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.select.Select;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -17,14 +20,26 @@ public class SqlAgent implements LoadSqlPort {
     private final JdbcTemplate jdbcTemplate;
 
     /**
-     * @param sql is a sql request for DB 
-     * @return
+     * @param sql is a sql request for DB
+     * @return json response in string format
      */
     @Override
     public String sendSqlPrompt(String sql) throws JsonProcessingException {
+        if (!isReadOnlyQuery(sql)) {
+            return "Your request show be read-only";
+        }
         List<Map<String, Object>> results = jdbcTemplate.queryForList(sql);
         SqlResponse sqlResponseDTO = new SqlResponse(results);
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.writeValueAsString(sqlResponseDTO);
+    }
+
+    private boolean isReadOnlyQuery(String sql) {
+        try {
+            Statement statement = CCJSqlParserUtil.parse(sql);
+            return statement instanceof Select;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Invalid SQL syntax", e);
+        }
     }
 }
