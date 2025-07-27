cd4c6f5930d237712fd5064cb554225a29b37b9d
MaxSazanov <makssazanov31@gmail.com>
2025-07-27 22:14:37 +0300

    configured notification service

--- email-notification-adapter/src/main/java/com/opencu/bookit/adapter/out/email/EmailNotificationAdapter.java
+++ email-notification-adapter/src/main/java/com/opencu/bookit/adapter/out/email/EmailNotificationAdapter.java
@@ -4,6 +4,7 @@ import com.opencu.bookit.application.port.out.nofication.NotificationPort;
 import com.opencu.bookit.domain.model.event.EventNotification;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
@@ -15,14 +16,21 @@ public class EmailNotificationAdapter implements NotificationPort {
 
     private final JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    @Value("${spring.mail.sender-name}")
+    private String senderName;
+
     @Override
     public void sendNotification(EventNotification notification) {
         log.info("Sending email notification to user: {} for event: {}",
                 notification.getUserId(), notification.getEventId());
 
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(String.format("%s <%s>", senderName, fromEmail));
         message.setTo(notification.getUserEmail());
-        message.setSubject("Напоминание о событии: " + notification.getEventTitle());
+        message.setSubject("Напоминание о мероприятии: " + notification.getEventTitle());
         message.setText(notification.getMessage());
 
         try {
