b7169a2898a5485bb57585b3df1ebf4af154d17e
MaxSazanov <makssazanov31@gmail.com>
2025-07-30 23:34:50 +0300

    endpoint for checking subscription availability + set true by default

--- web-adapter/src/main/java/com/opencu/bookit/adapter/in/web/controller/v1/NotificationControllerV1.java
+++ web-adapter/src/main/java/com/opencu/bookit/adapter/in/web/controller/v1/NotificationControllerV1.java
@@ -2,7 +2,6 @@ package com.opencu.bookit.adapter.in.web.controller.v1;
 
 import com.opencu.bookit.adapter.out.security.spring.service.AuthService;
 import com.opencu.bookit.application.service.nofication.NotificationService;
-import com.opencu.bookit.application.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,10 +17,12 @@ public class NotificationControllerV1 {
     private final NotificationService notificationService;
     private final AuthService authService;
 
-
     @PostMapping("/unsubscribe")
     public ResponseEntity<String> unsubscribe() {
         UUID userId = authService.getCurrentUser().getId();
+        if (!notificationService.isSubscribedToNotifications(userId)) {
+            return ResponseEntity.badRequest().body("You are already unsubscribed from notifications");
+        }
         notificationService.unsubscribeFromNotifications(userId);
         return ResponseEntity.status(HttpStatus.CREATED).body("You have successfully unsubscribed from notifications");
     }
@@ -34,5 +35,12 @@ public class NotificationControllerV1 {
         }
         notificationService.subscribeToNotifications(userId);
         return ResponseEntity.status(HttpStatus.CREATED).body("You have successfully subscribed to notifications");
+    }
+
+    @GetMapping("/check-subscription")
+    public ResponseEntity<Boolean> checkSubscription() {
+        UUID userId = authService.getCurrentUser().getId();
+        boolean isSubscribed = notificationService.isSubscribedToNotifications(userId);
+        return ResponseEntity.ok(isSubscribed);
     }
 }
