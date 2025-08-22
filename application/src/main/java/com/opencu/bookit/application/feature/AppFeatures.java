2989def03088ebd57a41af08e00789083cff90cf
#122FAA <makssazanov31@gmail.com>
2025-08-22 17:57:20 +0300

    togglz setup

---
+++ application/src/main/java/com/opencu/bookit/application/feature/AppFeatures.java
@@ -0,0 +1,15 @@
+package com.opencu.bookit.application.feature;
+
+import org.togglz.core.Feature;
+import org.togglz.core.annotation.EnabledByDefault;
+import org.togglz.core.context.FeatureContext;
+
+public enum AppFeatures implements Feature {
+
+    @EnabledByDefault
+    EMAIL_NOTIFICATIONS;
+
+    public boolean isActive() {
+        return FeatureContext.getFeatureManager().isActive(this);
+    }
+}
