52817e11a5c96d1b876b55005e1383c9e850a9d4
#122FAA <makssazanov31@gmail.com>
2025-08-22 17:58:31 +0300

    email toggle

---
+++ email-notification-adapter/src/main/java/com/opencu/bookit/adapter/out/email/spi/EmailProvider.java
@@ -0,0 +1,5 @@
+package com.opencu.bookit.adapter.out.email.spi;
+
+import com.opencu.bookit.application.port.out.nofication.NotificationPort;
+
+public interface EmailProvider extends NotificationPort {}
