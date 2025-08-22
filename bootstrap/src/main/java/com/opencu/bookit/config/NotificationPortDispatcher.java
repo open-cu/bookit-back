5183907c305cbc2cbc70e65cf86780ec50a1f4bf
#122FAA <makssazanov31@gmail.com>
2025-08-22 23:15:33 +0300

    feat: ConditionalOnProperty for EmailNotificationAdapter

--- bootstrap/src/main/java/com/opencu/bookit/config/NotificationPortDispatcher.java
+++ bootstrap/src/main/java/com/opencu/bookit/config/NotificationPortDispatcher.java
@@ -1,26 +1,29 @@
 package com.opencu.bookit.config;
 
 import com.opencu.bookit.adapter.out.email.spi.EmailProvider;
+import com.opencu.bookit.adapter.out.telegram.spi.TelegramProvider;
 import com.opencu.bookit.application.feature.AppFeatures;
 import com.opencu.bookit.application.port.out.nofication.NotificationPort;
 import com.opencu.bookit.domain.model.event.EventNotification;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
 @Primary
 public class NotificationPortDispatcher implements NotificationPort {
 
-    @Autowired
-    private final Map<AppFeatures, NotificationPort> adapters;
+    private final Map<AppFeatures, NotificationPort> adapters = new HashMap<>();
 
-    public NotificationPortDispatcher(Map<AppFeatures, NotificationPort> adapters) {
-        this.adapters = adapters;
+    @Autowired
+    public NotificationPortDispatcher(ObjectProvider<EmailProvider> emailProvider, ObjectProvider<TelegramProvider> telegramAdapterProvider) {
+        telegramAdapterProvider.ifAvailable(adapter -> adapters.put(AppFeatures.TELEGRAM_NOTIFICATIONS, adapter));
+        emailProvider.ifAvailable(adapter -> adapters.put(AppFeatures.EMAIL_NOTIFICATIONS, adapter));
     }
 
     @Override
