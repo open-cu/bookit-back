505262c3e6c24c78bd069033c81d2822f6342efc
Ravinskij-Vladimir <ravunskiy0412@gmail.com>
2025-08-22 13:30:25 +0300

    first steps to implement tg-notifications via tg bot

---
+++ telegram-bot-notification-adapter/src/main/java/com/opencu/bookit/adapter/out/telegram/TelegramBotConfig.java
@@ -0,0 +1,26 @@
+package com.opencu.bookit.adapter.out.telegram;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+@ConditionalOnProperty(value = "tg-bot.enabled")
+public class TelegramBotConfig {
+
+    private final MyTelegramBot myTelegramBot;
+
+    public TelegramBotConfig(MyTelegramBot myTelegramBot) {
+        this.myTelegramBot = myTelegramBot;
+    }
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi() throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(myTelegramBot);
+        return botsApi;
+    }
+}
